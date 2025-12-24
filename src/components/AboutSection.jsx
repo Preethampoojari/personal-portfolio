2dc7a3d4bc2ea53a27d8572d8316b45d049df5f3
Preetham Poojari <preethampoojari146@gmail.com>
2025-12-24 20:09:18 +0530

    Updated Resume Uploaded

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -40,7 +40,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="https://drive.google.com/file/d/1ue7zDWpxj2QP_KM20ainCtENQ36JI8N4/view?usp=drive_link"
+                href="https://drive.google.com/file/d/1rL0txD-9RgaCxVjuMUzuKkoLTFh6F1t8/view?usp=drive_link"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
